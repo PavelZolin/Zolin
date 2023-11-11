d75842c7634966ec3b7d2a655e6d435613bfb3ed
Pavel <138059065+PavelZolin@users.noreply.github.com>
2023-11-11 14:39:10 +0300

    Delete configuration.py

--- configuration.py
+++
@@ -1,5 +0,0 @@
-URL ="https://9c6c8c3c-2f50-47c0-9400-9bbe67b998f4.serverhub.praktikum-services.ru"
-CREATE_Courier = "/api/v1/courier"
-CREATE_Orders = "/api/v1/orders"
-track_Orders = "/api/v1/orders/track"
-
