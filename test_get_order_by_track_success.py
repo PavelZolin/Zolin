d71be600b67f21554794539fadbcb368e5021e2f
Pavel <138059065+PavelZolin@users.noreply.github.com>
2023-11-12 20:30:55 +0300

    Update test_get_order_by_track_success.py

--- test_get_order_by_track_success.py
+++ test_get_order_by_track_success.py
@@ -2,7 +2,7 @@ import sender_stand_request
 
 
 
-def test_positive_assert():
+def test_get_order_by_track_success():
     get_track = sender_stand_request.get_track_orders(sender_stand_request.get_track())
     assert get_track.status_code == 200
 
