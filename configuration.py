fbda286014981c4d3311a6466d6a4d28c71321f5
Pavel <138059065+PavelZolin@users.noreply.github.com>
2023-11-12 20:29:02 +0300

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,4 +1,3 @@
-URL ="https://dc2e1b8d-731e-4b2d-9e7b-5695f802e106.serverhub.praktikum-services.ru"
-Create_Orders = "/api/v1/orders"
-Track_Orders = "/api/v1/orders/track"
-
+URL ="https://3dfe0821-91b8-49c1-9648-96927c9f954f.serverhub.praktikum-services.ru"
+CREATE_ORDERS = "/api/v1/orders"
+TRACK_ORDERS = "/api/v1/orders/track"
